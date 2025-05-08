12d7c2336e9116331b20efaf2402358c744f37f3
k-stha1 <kritishshrestha123@gmail.com>
2025-05-09 01:16:10 +0545

    Initial Commit

---
+++ demo.py
@@ -0,0 +1,24 @@
+import string
+import random
+import my_module
+
+symbols=['@','#','$','&','!','^','*','~','+']
+alphabet_list = list(string.ascii_letters)
+value=""
+num=int(input("Chose the length of your password between 8 to 16 characters: "))
+
+if num>=8 and num<=16:
+    if num%2 ==0:
+        for i in range ((num//2)-1):
+            value +=random.choice(alphabet_list) + str(random.choice(my_module.numbers))
+        value += random.choice(symbols) + random.choice(symbols)
+        print(value)
+    
+    else:
+        for i in range ((num-1)//2):
+            value +=random.choice(alphabet_list) + str(random.choice(my_module.numbers))
+        value += random.choice(symbols)
+        print(value)
+else:
+    print("Invalid")
+       
